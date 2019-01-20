53b7573f902882b2f68ca638cf9c4e6d95936107
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2019-01-20 21:36:11 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,39 +10,26 @@ public class Main {
 		 * : You don't access to the static variable (or the method) - numberInstances -
 		 * this way: city1.getNbrInstances() but rather referring to the class - City -
 		 */
+		
+		// to access a public class variable
+		System.out.println("The number of cities we've created is: " + City.numberInstancesPublic + " (public counter)");
+		
+		// to access a private class variable
+		System.out.println("The number of cities we've created is: " + City.getNbrInstances() + " (public counter)"); 
 
-		System.out
-				.println("The number of cities we've created is: " + City.numberInstancesPublic + " (public counter)");// to
-																														// access
-																														// a
-																														// public
-																														// class
-																														// variable
-		System.out.println("The number of cities we've created is: " + City.getNbrInstances() + " (public counter)"); // to
-																														// access
-																														// a
-																														// private
-																														// class
-																														// variable
-
-		System.out
-				.println("The first City is: " + city1.getNameCity() + ", & it's located in " + city1.getNameCountry());
+		System.out.println("The first City is: " + city1.getNameCity() + ", & it's located in " + city1.getNameCountry());
 		System.out.println("The number of its inhabitants is: " + city1.getNbInhabitants());
 
 		City city2 = new City("Marseille", "France", 1059000);
-		System.out
-				.println("The number of cities we've created is: " + City.numberInstancesPublic + " (public counter)");
+		System.out.println("The number of cities we've created is: " + City.numberInstancesPublic + " (public counter)");
 		System.out.println("The number of cities we've created is: " + City.getNbrInstances() + " (private counter)");
-		System.out.println(
-				"The second City is: " + city2.getNameCity() + ", & it's located in " + city2.getNameCountry());
+		System.out.println("The second City is: " + city2.getNameCity() + ", & it's located in " + city2.getNameCountry());
 		System.out.println("The number of its inhabitants is: " + city2.getNbInhabitants());
 
 		City city3 = new City("Köln", "Germany", 1046680);
-		System.out
-				.println("The number of cities we've created is: " + City.numberInstancesPublic + " (public counter)");
+		System.out.println("The number of cities we've created is: " + City.numberInstancesPublic + " (public counter)");
 		System.out.println("The number of cities we've created is: " + City.getNbrInstances() + " (public counter)");
-		System.out
-				.println("The third City is: " + city3.getNameCity() + ", & it's located in " + city3.getNameCountry());
+		System.out.println("The third City is: " + city3.getNameCity() + ", & it's located in " + city3.getNameCountry());
 		System.out.println("The number of its inhabitants is: " + city3.getNbInhabitants());
 		System.out.println();
 
@@ -54,14 +41,11 @@ public class Main {
 		temporary = city1;
 		city1 = city2;
 		city2 = temporary;
-		System.out.println(
-				"The first City is: " + city1.getNameCity() + ", & it's located in " + city1.getNameCountry() + ".");
+		System.out.println("The first City is: " + city1.getNameCity() + ", & it's located in " + city1.getNameCountry() + ".");
 		System.out.println("The number of its inhabitants is: " + city1.getNbInhabitants() + ".\n");
-		System.out.println(
-				"The second City is: " + city2.getNameCity() + ", & it's located in " + city2.getNameCountry() + ".");
+		System.out.println("The second City is: " + city2.getNameCity() + ", & it's located in " + city2.getNameCountry() + ".");
 		System.out.println("The number of its inhabitants is: " + city2.getNbInhabitants() + ".\n");
-		System.out.println(
-				"The third City is: " + city3.getNameCity() + ", & it's located in " + city3.getNameCountry() + ".");
+		System.out.println("The third City is: " + city3.getNameCity() + ", & it's located in " + city3.getNameCountry() + ".");
 		System.out.println("The number of its inhabitants is: " + city3.getNbInhabitants() + ".\n");
 
 		/**
@@ -70,11 +54,9 @@ public class Main {
 		city1.setNameCity("Honk Kong");
 		city2.setNameCity("Tatooine");
 		System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-		System.out.println(
-				"\nThe first City is: " + city1.getNameCity() + ", & it's located in " + city1.getNameCountry() + ".");
+		System.out.println("\nThe first City is: " + city1.getNameCity() + ", & it's located in " + city1.getNameCountry() + ".");
 		System.out.println("The number of its inhabitants is: " + city1.getNbInhabitants() + ".\n");
-		System.out.println(
-				"The second City is: " + city2.getNameCity() + ", & it's located in " + city2.getNameCountry() + ".");
+		System.out.println("The second City is: " + city2.getNameCity() + ", & it's located in " + city2.getNameCountry() + ".");
 		System.out.println("The number of its inhabitants is: " + city2.getNbInhabitants() + ".\n");
 
 		/**
@@ -86,11 +68,9 @@ public class Main {
 		city2.setNameCountry("Tunisia");
 		city2.setNbInhabitants(67291);
 		System.out.println("|**Rectification**|");
-		System.out.println(
-				"\nThe first City is: " + city1.getNameCity() + ", & it's located in " + city1.getNameCountry() + ".");
+		System.out.println("\nThe first City is: " + city1.getNameCity() + ", & it's located in " + city1.getNameCountry() + ".");
 		System.out.println("The number of its inhabitants is: " + city1.getNbInhabitants() + ".\n");
-		System.out.println(
-				"The second City is: " + city2.getNameCity() + ", & it's located in " + city2.getNameCountry() + ".");
+		System.out.println("The second City is: " + city2.getNameCity() + ", & it's located in " + city2.getNameCountry() + ".");
 		System.out.println("The number of its inhabitants is: " + city2.getNbInhabitants() + ".\n");
 		System.out.println("______\nSo ...");
 		System.out.println(city1.cityDescription());
