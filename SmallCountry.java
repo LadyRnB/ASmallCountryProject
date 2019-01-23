cc6de7c7fa0405b75c02abc9e4fbbd524b2f03db
R'n'B <9968920+RnBDeveloper@users.noreply.github.com>
2019-01-23 01:00:23 +0100

    Add files via upload

--- SmallCountry.java
+++ SmallCountry.java
@@ -2,12 +2,12 @@ public class SmallCountry {
 	public static void main(String[] args) {
 		City c = null;
 		try {
-			c = new City("re", "France", 12000);
+			c = new City("re", "France", -12000);
 		} catch (NumberInhabitantsException | CityNameException e2) {
 			e2.printStackTrace();
 			//This method "printStackTrace" helps to understand where the actual problem
 			//occurred. it's equivalent to: - System.out.println(e2); -
-			System.out.println(e2.getMessage());
+			System.out.println("\n" + e2.getMessage());
 		}
 
 		finally {
